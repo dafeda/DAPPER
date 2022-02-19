52e31a068a3e63fa192052b8eafdf59e78c511e4
Feda Curic <feda.curic@gmail.com>
2022-02-19 12:50:20 +0100

    Use run_forward instead of recursion in LA demo

--- dapper/mods/LA/demo.py
+++ dapper/mods/LA/demo.py
@@ -5,11 +5,9 @@ import dapper.mods as modelling
 from dapper.mods.LA.raanes2015 import X0, step
 from dapper.tools.viz import amplitude_animation
 
-simulator = modelling.with_recursion(step, prog="Simulating")
-
 x0 = X0.sample(1).squeeze()
 dt = 1
-xx = simulator(x0, k=500, t=0, dt=dt)
+xx = modelling.run_forward(step, nsteps=500, x=x0, t=0, dt=dt, prog="Simulating")
 
 anim = amplitude_animation(xx, dt)
 plt.show()
