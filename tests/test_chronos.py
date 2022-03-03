a09b5cb3a9363b40ecd3ca65b2f97e741bcb643c
Feda Curic <feda.curic@gmail.com>
2022-03-03 10:25:33 +0100

    Fix broken chronos tests

--- tests/test_chronos.py
+++ tests/test_chronos.py
@@ -11,11 +11,7 @@ def test_chronology_replacement():
     K = round(T / dt)
     dko = 5
     tseq = modelling.Chronology(dt=dt, dko=dko, T=T, BurnIn=-1, Tplot=100)
-    # obs_mask is zero for time epochs where we don't have a observation,
-    # and one when we do have a observation.
-    obs_mask = np.zeros(K, dtype=int)
-    obs_mask[np.arange(dko, K + dko, dko) - 1] = 1
-    tseq2 = modelling.Chronology2(dt=1, T=300, obs_mask=obs_mask)
+    tseq2 = modelling.Chronology2(dt=1, T=300, dto=dko * dt)
 
     print(tseq2.kko)
 
@@ -28,15 +24,11 @@ def test_ticker():
     dt = 1
     K = 10
     dko = 2
-    obs_mask = np.zeros(K, dtype=int)
-    obs_mask[np.arange(dko, K + dko, dko) - 1] = 1
-    kko = np.where(obs_mask == 1)[0] + 1
     kk = np.arange(K + 1)
     tt = kk * dt
 
-    tckr = Ticker(tt, kko)
-
-    tseq = modelling.Chronology2(dt=dt, T=K, obs_mask=obs_mask)
+    tseq = modelling.Chronology2(dt=dt, T=K, dto=dko * dt)
+    tckr = Ticker(tt, tseq.kko)
 
     next(tckr)
     for ticker in tseq.ticker:
